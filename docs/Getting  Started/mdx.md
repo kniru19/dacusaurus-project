1cb33c4b24fd5197717b25382c40d8a8044ab4b7
kniru19 <kniru19@gmail.com>
2025-03-10 22:27:21 +0200

    updated agian all

---
+++ docs/Getting  Started/mdx.md
@@ -0,0 +1,10 @@
+---
+title: MDX
+sidebar_position: 3
+---
+
+import Tag from '@site/src/components/Tag';
+
+
+
+I can write **Markdown** alongside my _JSX_!
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,6 +89,7 @@ const config = {
             label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
+          { to: 'about', label: 'About', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
